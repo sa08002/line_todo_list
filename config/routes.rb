41148c070286670a28c53c4e2d133c58b7b93b76
大西幹則 <1078sa08002@gmail.com>
2020-08-01 14:41:19 +0900

    LINE で送ったメッセージをタスクとして登録する API を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :tasks
 
   root "tasks#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/callback" => "line_bot#callback"
+
 end
